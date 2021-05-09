831075e642a5e953b4e1d9022ce5966bdbdc54ae
nochphanith <nochphanith@gmail.com>
2021-05-09 12:31:21 +0700

    Initial Commit

---
+++ PhoneTextFieldExample/PhoneTextFieldExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhoneTextFieldExampleApp.swift
+//  PhoneTextFieldExample
+//
+//  Created by Admin on 5/9/21.
+//
+
+import SwiftUI
+
+@main
+struct PhoneTextFieldExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
